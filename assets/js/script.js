a251d6a058099db607be10e1dbf6712137b88797
Edgar Juarez <ejjt.686@gmail.com>
2017-07-04 13:34:33 -0700

    Added icons and styled the rest. Also added animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,6 @@ $('input[type="text"]').keypress(function(e) {
     if(e.which === 13) {
         let newTodo = $(this).val();
         $(this).val('');
-        $('ul').append(`<li><span>X</span>${newTodo}</li>`);
+        $('ul').append(`<li><span><i class="fa fa-trash"></i></span>${newTodo}</li>`);
     }
 });
