89ddb97890b92d5ceff3a497c1d15ea8ccd33d24
Edgar Juarez <ejjt.686@gmail.com>
2017-07-04 12:08:20 -0700

    Clicking on the X now removes the li

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@
 $('li').click(function () {
     $(this).toggleClass('completed');
 });
+
+// Click on X to delete todos
+$('span').click(function (e) {
+    $(this).parent().fadeOut(function() {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
